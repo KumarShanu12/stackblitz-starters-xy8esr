3674407e66327ae97d4969765f5641ec9c5064d2
KumarShanu12 <32716103+KumarShanu12@users.noreply.github.com>
2023-06-09 22:46:50 +0300

    2 files modified

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,55 @@
 import * as React from 'react';
+import axios from axios;
 const Home = (props) => {
- console.log('prop',props)
+
+  
+  console.log('prop', props);
   return (
     <>
       <div>
+
+     {/* Header */}
+
+
+<div>
+     <div>
+       {/* Routing */}
+     </div> 
+
+     <div>
+       {/* Product */}
+     </div> 
+    
+     <div>
+       {/* Add to cart */}
+     </div> 
+
+</div>  
+
+     
+     
+
+     
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <h1>Home Component</h1>
         <div className="cart-wrapper">
           <div className="img-wrapper item">
@@ -16,12 +62,25 @@ const Home = (props) => {
           <div className="btn-wrapper item">
             <button
               onClick={() => {
-                props.addToCartHandler({ pice: 1000, name: 'i phone 11' }  );
+                props.addToCartHandler({
+                  id: 1,
+                  title:
+                    'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+                  price: 109.95,
+                  description:
+                    'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+                  category: "men's clothing",
+                  image:
+                    'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+                  rating: {
+                    rate: 3.9,
+                    count: 120,
+                  },
+                });
               }}
             >
               Add To Cart
             </button>
-
 
             <button
               onClick={() => {
